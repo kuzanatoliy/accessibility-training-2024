75fc1c1d4fb5726902872c0b99ea69eaa8d07985
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-05-15 12:28:04 +0300

    init: clone repository from https://git.epam.com/Vera_Barysava/accessibility-training

---
+++ README.md
@@ -0,0 +1,13 @@
+## Prerequisites
+Use node >=18.12.0 to support the current style-lint version or downgrade style-lint
+
+
+## How to run project
+* Make `git clone`
+* Install dependencies `npm ci`
+* Run webpack-dev-server `npm run start`
+* Open url `localhost:9000` in your browser
+
+### Notes
+
+The project has a commit check in place. A commit won't be possible until main errors are fixed.
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import './styles/index.scss';
+
+// tabs
+const tabs = document.querySelector('.section.section-no-bottom-padding.is-bg-interactive');
+const tabButtons = tabs.querySelectorAll('.tab');
+const tabPanels = tabs.querySelectorAll('.tab-panel');
+
+function handleTabClick(event) {
+  // hide all tab panels
+  tabPanels.forEach(panel => panel.hidden = true);
+  // mark all tabs as unselected
+  tabButtons.forEach(tab => tab.classList.remove('is-active'));
+  // mark the clicked tab as selected
+  this.classList.add('is-active');
+  // find the associated tabPanel and show it
+  const { id } = event.currentTarget;
+  const tabPanel = tabs.querySelector(`[data-panel="${id}"]`);
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
+
+const modalButtons = document.querySelectorAll('.modal-button');
+const modalCloseButtons = document.querySelectorAll('.modal-close');
+
+function showModal(event) {
+  event.stopImmediatePropagation();
+  const target = document.getElementById(event.target.attributes['data-target'].value);
+  target && target.classList.toggle('hidden');
+}
+
+modalButtons.forEach(button => button.addEventListener('click', showModal));
+modalCloseButtons.forEach(button => button.addEventListener('click', showModal));
